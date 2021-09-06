3976e5fb2a631a650279fdfaed7e8137bf250f29
Sara Evans <saraevs@gmail.com>
2021-09-06 23:36:03 +0100

    feat: create gif component and add selected gif to the index

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 // internal
 import SearchBar from './search-bar.jsx';
+import Gif from './gif.jsx';
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
       <div>
         <div className="left-scene">
           <SearchBar></SearchBar>
+          <div className="selected-gif">
+            <Gif Id='xT9IgDEI1iZyb2wqo8'></Gif>
+          </div>
         </div>
         <div className="right-scene"></div>
       </div>
---
+++ src/components/gif.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    const src = `https://media2.giphy.com/media/${this.props.id}/giphy.gif`
+
+    return (
+      <img src={src} alt="" className="gif" />
+    )
+  }
+
+}
+
+export default Gif;
