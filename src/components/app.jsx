35c857cc4f6f5085da1f7798b23082ab1618fe14
Sara Evans <saraevs@gmail.com>
2021-09-07 00:06:43 +0100

    feat: add giflist component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,18 +4,25 @@ import React, { Component } from 'react';
 // internal
 import SearchBar from './search-bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif-list.jsx';
 
 class App extends Component {
   render() {
+    const gifs = [
+      { id: 'BBkKEBJkmFbTG'} ,
+      { id: 'xT9IgDEI1iZyb2wqo8' }
+    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar></SearchBar>
           <div className="selected-gif">
-            <Gif Id='xT9IgDEI1iZyb2wqo8'></Gif>
+            <Gif Id='xT9IgDEI1iZyb2wqo8' />
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs}/>
+        </div>
       </div>
     )
   }
