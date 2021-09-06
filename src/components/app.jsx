02b1ff91fd0f0b80834316e01c8599d809ffc631
Sara Evans <saraevs@gmail.com>
2021-09-06 23:07:53 +0100

    feat: create searchbar component"

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,16 @@
+// external
 import React, { Component } from 'react';
+
+// internal
+import SearchBar from './search-bar.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="left-scene"></div>
+        <div className="left-scene">
+          <SearchBar></SearchBar>
+        </div>
         <div className="right-scene"></div>
       </div>
     )
